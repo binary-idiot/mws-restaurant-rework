67aef6815a606689165e2a5e7d6714e6d04bdf6a
Jonah Middleton <jonah.middleton@gmail.com>
2018-08-28 02:12:27 -0400

    add map to details page

--- src/js/details.js
+++ src/js/details.js
@@ -1,6 +1,8 @@
 let restaurant,
 	worker;
 
+var newMap;
+
 	document.addEventListener('DOMContentLoaded', event => {
 		// worker to handle all restaurant retrieval
 		this.worker = new Worker('/src/js/restaurantWorker.js');
@@ -19,12 +21,37 @@ handleWorkerMessage = msg => {
 	switch(data.retrieved){
 		case 'restaurant':
 			self.restaurant = content;
-			fillRestaurantHTML();
-			fillBreadcrumb();
+			requestAnimationFrame(initMap);
+			requestAnimationFrame(fillRestaurantHTML);
 			break;
 	}
 }
 
+initMap = () => {
+	const restaurant = self.restaurant;
+	self.newMap = L.map('map', {
+	    center: [restaurant.latlng.lat, restaurant.latlng.lng],
+	    zoom: 16,
+	    scrollWheelZoom: false
+	  });
+	  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+	    mapboxToken: 'pk.eyJ1IjoiYmluYXJ5aWRpb3QiLCJhIjoiY2pqMzZjNWRtMWF2YTNrbXRsb2VueGlydyJ9.mkjp31-552zW210Dz1PUcQ',
+	    maxZoom: 18,
+	    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+	      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+	      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	    id: 'mapbox.streets'    
+	  }).addTo(newMap);
+
+	  Helper.mapMarkerForRestaurant(self.restaurant, self.newMap);
+}
+
+/**
+ * get value of url param
+ * @param  {String} name param to search for
+ * @param  {String} url  url to search for
+ * @return {String}      Value of param
+ */
 getParameterByName = (name, url) => {
   if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, '\\$&');
@@ -44,7 +71,13 @@ getRestaurant = (id, worker = self.worker) => {
 	worker.postMessage({action: 'getRestaurant', id: id});
 }
 
-fillRestaurantHTML = (restaurant = self.restaurant) => {
+/**
+ * Fill restaurant page with restaurant data
+ * @param  {Json} restaurant Restaurant to fill page with
+ */
+fillRestaurantHTML = () => {
+	const restaurant = self.restaurant;
+
 	const name = document.getElementById('restaurant-name');
 	name.innerHTML = restaurant.name;
 
@@ -68,8 +101,13 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 	}
 
 	//reviews
+	fillBreadcrumb();
 }
 
+/**
+ * Fill operating hours section
+ * @param  {Json} operatingHours Operating hours to add to page
+ */
 fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => {
 	const hours = document.getElementById('restaurant-hours');
 	for (let key in operatingHours) {
@@ -88,7 +126,13 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
 	}
 }
 
-fillBreadcrumb = (restaurant = self.restaurant) => {
+/**
+ * Fill page breadcrumb
+ * @param  {Json} restaurant Restaurant for page
+ */
+fillBreadcrumb = () => {
+	const restaurant = self.restaurant;
+	
 	const breadcrumb = document.getElementById('breadcrumb');
 	const li = document.createElement('li');
 	li.innerHTML = restaurant.name;
