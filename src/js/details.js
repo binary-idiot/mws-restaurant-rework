207cd8adff4a26bb78075a4af482126ca461291f
Jonah Middleton <jonah.middleton@gmail.com>
2018-08-28 01:27:49 -0400

    fill restaurant html on details page

--- src/js/details.js
+++ src/js/details.js
@@ -18,23 +18,21 @@ handleWorkerMessage = msg => {
 
 	switch(data.retrieved){
 		case 'restaurant':
-			console.log(content);
+			self.restaurant = content;
+			fillRestaurantHTML();
+			fillBreadcrumb();
 			break;
 	}
 }
 
 getParameterByName = (name, url) => {
-  if (!url)
-    url = window.location.href;
-  name = name.replace(/[\[\]]/g, '\\$&');
-  const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
-    results = regex.exec(url);
-  if (!results)
-  	console.error(`Error no ${name} in url`);
-    return null;
-  if (!results[2])
-    return '';
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
 /**
@@ -44,4 +42,55 @@ getParameterByName = (name, url) => {
  */
 getRestaurant = (id, worker = self.worker) => {
 	worker.postMessage({action: 'getRestaurant', id: id});
+}
+
+fillRestaurantHTML = (restaurant = self.restaurant) => {
+	const name = document.getElementById('restaurant-name');
+	name.innerHTML = restaurant.name;
+
+	const address = document.getElementById('restaurant-address');
+	address.innerHTML = restaurant.address;
+
+	const image = document.getElementById('restaurant-img');
+	const imgSrc =  Helper.imageUrlForRestaurant(restaurant);
+	image.className = 'restaurant-img'
+	image.src = `${ imgSrc }-small.jpg`;
+	image.srcset = `${ imgSrc }-small.jpg 300w, ${ imgSrc }-medium.jpg 600w, ${ imgSrc }-large.jpg 800w`;
+	image.size = '(max-width: 767) calc(100% - 30px), calc(50% - 30px)';
+	image.alt = restaurant.alt;
+
+	const cuisine = document.getElementById('restaurant-cuisine');
+	cuisine.innerHTML = restaurant.cuisine_type;
+
+	// fill operating hours
+	if (restaurant.operating_hours) {
+	fillRestaurantHoursHTML();
+	}
+
+	//reviews
+}
+
+fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => {
+	const hours = document.getElementById('restaurant-hours');
+	for (let key in operatingHours) {
+		const row = document.createElement('tr');
+
+		const day = document.createElement('td');
+		day.innerHTML = key;
+		row.appendChild(day);
+
+		const time = document.createElement('td');
+		const oh = operatingHours[key].replace(/,/, '<br>');
+		time.innerHTML = oh;
+		row.appendChild(time);
+
+		hours.appendChild(row);
+	}
+}
+
+fillBreadcrumb = (restaurant = self.restaurant) => {
+	const breadcrumb = document.getElementById('breadcrumb');
+	const li = document.createElement('li');
+	li.innerHTML = restaurant.name;
+	breadcrumb.appendChild(li);
 }
